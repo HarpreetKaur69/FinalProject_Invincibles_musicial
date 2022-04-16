ea0e5da4de3b476e1af80ecadb13f76ae09ab9c0
HarpreetKaur69 <c0836169@mylambton.ca>
2022-04-17 00:26:06 +0530

    Home Page Responsive and Layout  With Stikcy Navbar
    Sticky Navbar Fixed in small Screen and Color Theme purple

---
+++ assests/js/mainjs.js
@@ -0,0 +1,5 @@
+$(function(){
+    $("button").on('click',()=>{
+        $(".audio").attr('src','assests/mp3/Jim-Yosef-Lights-NCS-Release.mp3');
+    })
+})
--- assets/js/mainjs.js
+++
@@ -1,5 +0,0 @@
-$(function(){
-    $("button").on('click',()=>{
-        $(".audio").attr('src','assets/mp3/Jim-Yosef-Lights-NCS-Release.mp3');
-    })
-})
