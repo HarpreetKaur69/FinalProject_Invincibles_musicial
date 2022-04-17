cc0f33510accbd4b233d0603ebc2c1d7a4bd9edc
HarpreetKaur69 <c0836169@mylambton.ca>
2022-04-17 14:40:10 +0530

    Contact Page with validation and submitted jqeury
    Responsive contact with juqery

--- assests/js/mainjs.js
+++ assests/js/mainjs.js
@@ -1,5 +1,9 @@
-$(function(){
-    $("button").on('click',()=>{
-        $(".audio").attr('src','assests/mp3/Jim-Yosef-Lights-NCS-Release.mp3');
-    })
-})
+// $(function(){
+//     $("button").on('click',()=>{
+//         $(".audio").attr('src','assests/mp3/Jim-Yosef-Lights-NCS-Release.mp3');
+//     })
+// })
+$(document).ready(function(){
+    $('#nav_menu').slicknav({prependTo:"#mobile_menu"});
+    
+});
